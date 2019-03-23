22be4e28d6c9a76366bd2bebab084027e0e926ec
NiskuT <qjupi63@gmail.com>
2019-03-23 16:54:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ class NeuralNetwork:
 
 		self.weights = []
 		for k in range(1,len(args)):
-			self.weights.append((2*np.random.random((args[k],args[(k-1)]))-1))
+			self.weights.append([(2*np.random.random((args[k],args[(k-1)]))-1), (2*np.random.random((args[k],1))-1)])
 
 	def sigmoid(self, x):
 		return (1/(1+np.exp(-x)))
@@ -17,11 +17,25 @@ class NeuralNetwork:
 	def forward(self, data):
 
 		for w in self.weights:
+			try:
+				data.append(self.sigmoid(np.dot(w[0],data[-1]) + w[1]))
+			except ValueError:
+				print("Erreur sur les valeurs d'entrée.")
 
-			data = np.append(data)
+		return data[-1]
 
+	def cost(self, data, wanted):
+		return (data-wanted)**2
+
+	def backprop(self, data, wanted):
 		return 0
 
-test = NeuralNetwork(11,4,7)
+
+
+
+
+test = NeuralNetwork(3,2)
 for k in test.weights:
 	print(k)
+
+print(test.forward(    [   np.random.random((3, 1))]       )     )
