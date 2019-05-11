97253e79b4e19ea5810a032a5e8809fab227ca4c
NiskuT <qjupi63@gmail.com>
2019-05-11 13:03:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,97 +1,104 @@
+# -*- coding: utf-8 -*-
+
+
 import numpy as np
 import random as rd
 
+
 class NeuralNetwork:
-	
-	def __init__(self, *args):
-
-		self.weights = []
-		self.app = 0.05
-		for k in range(1,len(args)):
-			self.weights.append([(2*np.random.random((args[k],args[(k-1)]))-1), (2*np.random.random((args[k],1))-1)])
-
-	def sigmoid(self, x):
-		return (1/(1+np.exp(-x)))
-
-	def Dsigm(self, x):
-		return x*(1-x)
-
-	def forward(self, data):
-
-		for w in self.weights:
-			try:
-				data.append(self.sigmoid(np.dot(w[0],data[-1]) + w[1]))
-			except ValueError:
-				print("Erreur sur les valeurs.")
-		return data
-
-
-	def cost(self, data, wanted):
-		return (data-wanted)**2
-
-	def backprop(self, data, wanted):
-		
-		a=self.cost(data[-1], wanted)
-		delta = [a]
-		deltaP = [self.cost(data[-1], wanted)]
-
-		for i in range(1, len(data)):
-
-			delta.append( self.Dsigm(data[-i-1])*np.dot(self.weights[-i][0].T, delta[-1]) )
-			deltaP.append( np.dot(self.weights[-i][1], delta[-1].T) )
-
-		delta.reverse()
-		deltaP.reverse()
-		
-		for i in range(len(self.weights)):
-			for j in range(len(self.weights[i][0])):
-				for k in range(len(self.weights[i][0][j])):
-					self.weights[i][0][j,k] += data[i][k] * self.app * delta[i+1][j]
-
-		for i in range(len(self.weights)):
-			for j in range(len(self.weights[i][1])):
-				for k in range(len(self.weights[i][1][j])):
-					self.weights[i][1][j,k] +=  self.app * delta[i+1][j]
-
-		return a
-
-	def train(self):
-		a= int(rd.getrandbits(1))
-		b= int(rd.getrandbits(1))
-		inpuT = np.array([[a],[b]])
-		output = self.forward([inpuT])
-		wanted = np.array([[a != b], [a^b]])
-
-		a = self.backprop(output, wanted)
-		return a
-
-
-	def loop(self, n, e = 50):
-		i=0
-		for k in range(n):
-			i+=1
-			if i > e:
-				print(self.train(), "numero", k)
-				i=0
-			else:
-				self.train()
+    
+    def __init__(self, *args):
+        """
+            Entrer en paramètre le nombre de neuronnes par couche
+            
+            Le tableau weights contient deux colonnes:
+            colonne 0: les matrices de poids entre deux couches, cad chaque matrice
+            contient dans la colonne k les poids reliés aux neuronnes d'entrée k
+            et la ligne l les poids reliés aux neuronnes d'arrivée l
+                
+            colonne 1: les biais en matrice colonne
+            
+            app: taux d'apprentissage du réseau
+        """      
+        
+        self.weights = []
+        self.app = 0.05
+        
+        for k in range(1,len(args)):
+            
+            self.weights.append([(2*np.random.random((args[k],args[(k-1)]))-1), (2*np.random.random((args[k],1))-1)])            
 
 
 
+    def sigmoid(self, x):
+        return (1/(1+np.exp(-x)))
 
 
+    def forward(self, inp):
+        """
+            Fonction principal du réseau:
+            Prend en paramètres un ndarray colonne de meme taille que la couche 1
+            
+            Renvoie un tableau contenant la valeur de chaque neuronne
+            Utiliser data[-1] pour obtenir la sortie final
+        
+        """
+        data = [inp]
+        for w in self.weights:
+            try:
+                data.append(self.sigmoid(np.dot(w[0],data[-1]) + w[1]))
+            except ValueError:
+                print("Erreur sur les valeurs.")
+                
+        return data
+
+    def cost(self, data, wanted):
+        """ 
+        Fonction de cout
+        squared error function
+        """
+        r= data-wanted
+        a=r*r
+        b=0.5*a
+        return b
+
+    def backprop(self, data, wanted):
+
+        miss = [self.cost(data[-1], wanted)]
+
+        for k in range(1,len(self.weights)+1):
+            
+            der=data[-k]*(1.0-data[-k])
 
 
-first = NeuralNetwork(2,4,4,2)
-first.loop(30000, 1000)
-print("les test!")
-print(first.forward([np.array([[1], [1]])])[-1])
-print(first.forward([np.array([[0], [1]])])[-1])
-print(first.forward([np.array([[1], [0]])])[-1])
-print(first.forward([np.array([[0], [0]])])[-1])
-#for k in test.weights:
-#	print(k)
+            chgt=miss[-1]*der
 
-#print( "\n\n\n\n", test.backprop(test.forward([np.random.random((3, 1))]), np.array([[0.7], [0.]])) )
+            deltaBias = self.app*np.dot
+            delta= self.app * np.dot(chgt, data[-k-1].T)
 
-#http://www.anyflo.com/bret/cours/rn/rn5.htm#exemple
+            self.weights[-k][0] += delta
+            print("Modif sur le poids:", len(self.weights)-k)
+            miss.append(np.dot(self.weights[-k][0].T, miss[-1]))
+
+        return 0
+
+
+      
+#if __name__ == "__main__":
+    
+N = NeuralNetwork(3,4,4,2)
+
+a = N.forward(np.array([[2],[3],[4]]))
+print(a)
+for k in N.weights:
+    print("\n\n")
+    print(k[0])
+
+print("C partie!")
+
+N.backprop(a, np.array([[0.3],[0.5]]))
+print("Done")
+
+for k in N.weights:
+    print("\n\n")
+    print(k[0])
