6db2fa7e575b7d9d755373b3939a02bd54b90986
NiskuT <qjupi63@gmail.com>
2019-10-19 22:19:53 +0200

    Add files via upload

---
+++ CNN.py
@@ -0,0 +1,145 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class CNN:
+    
+    def __init__(self, schema=[] ):
+        '''
+            Prend en paramètre le schema du CNN sous forme de liste:
+            Pour une etape de convultion:
+                "c[nbDeFiltre]" ex: "c8" pour une convultion avec 8 filtres
+            Pour une etape de pooling:
+                "pm[dim]" pour un max pooling ex: "pm2" pour un pooling "2x2"
+                "pa[dim]" pour un average pooling : "pa3" pour un pooling "3x3"
+            Pour une etape de non-linéarisation (qui evite notamment le 
+            sur-apprentissage):
+                "r" pour relu
+                "s" pour sigmoid
+                            
+        '''
+        self.schema = schema
+
+        
+        
+        
+    def sigmoid(self,x):
+        return (1/(1+np.exp(x)))
+    
+    def relu(self,x):
+        return max(0,x)
+    
+    def pooling(self, Type, imageBank, dim):
+        """
+            Fonction de pooling pour image 2D (nuance de gris)
+            type: m pour max pooling 
+                  a pour average pooling
+            imageBank est une image de profondeur le nombre d'images 
+            = une liste de matrices 2D
+        """
+        hpadding = imageBank.shape[0]%dim
+        vpadding = imageBank.shape[1]%dim
+        print("hpadding:",hpadding)
+        print("vpadding:",vpadding)
+        print(imageBank.shape)
+        
+        
+        newBank = np.zeros(((imageBank.shape[0]+hpadding)//dim,\
+                            (imageBank.shape[1]+vpadding)//dim, imageBank.shape[2]))
+
+        for k in range(newBank.shape[2]):
+            im = np.zeros(((imageBank.shape[0]+hpadding),(imageBank.shape[1]+vpadding)))
+            print(imageBank[:,:,k].shape)
+            print(im[hpadding//2:im.shape[0]-(hpadding//2+hpadding%2),vpadding//2:im.shape[1]-(vpadding//2+vpadding%2)].shape)
+            im[hpadding//2:im.shape[0]-(hpadding//2+hpadding%2),vpadding//2:im.shape[1]-(vpadding//2+vpadding%2)]\
+            += imageBank[:,:,k]
+            for i in range(newBank.shape[0]):
+                for j in range(newBank.shape[1]):
+                    if Type == "m":
+                        newBank[i,j,k]=np.max(im[dim*i:dim*(i+1), dim*j:dim*(j+1)])
+                    elif Type == "a":
+                        newBank[i,j,k]=np.mean(im[dim*i:dim*(i+1), dim*j:dim*(j+1)])
+        return newBank
+    
+    
+    
+    def convolution(self, imageBank, filterBank):
+        '''
+            imageBank: une matrice avec comme profondeur le nombre d'images 2D
+            à traiter. Pour la première convolution on traite indépendent R,G,B
+            Attention: il s'agit obligatoirement d'une matrice 3D
+            
+            filterBank: une matrice de taille n,n,y avec n la dimension des filtres
+            et y le nombre de filtres
+        '''
+
+        # verification des filtres
+        if filterBank.shape[0] != filterBank.shape[1] or \
+        filterBank.shape[0] > imageBank.shape[0] or \
+        filterBank.shape[0]%2==0 or len(filterBank.shape)!=3:
+            # On attend des filtres qu'ils soient de dimension inférieur à celle
+            # de l'image, carrés, et de dimension impaire (pour avoir un pixel centré)
+            # On attend également que filterBank soit une matrice 3D
+            raise NameError("Erreur_filtre")
+            
+        output = []
+        
+        for image in range(imageBank.shape[2]):
+            for filtre in range(filterBank.shape[2]):
+                output.append(self.convWithSingleFilter(imageBank[:,:,image], filterBank[:,:,filtre]))
+            
+        return np.transpose(np.array(output), axes=(1,2,0))
+        
+        
+    def convWithSingleFilter(self, img, filter1):
+        
+        # création de l'image de sorie
+        out = np.zeros(((img.shape[0]-filter1.shape[0]+1),(img.shape[1]-filter1.shape[1]+1)))
+        
+        for k in range(out.shape[0]):
+            for i in range(out.shape[1]):
+                try:
+                    for j in range(img.shape[2]):
+                        #print(k,i,j)
+                        #print(img[k:filter1.shape[0], i:filter1.shape[1],j])
+                        out[k,i]+=np.sum(img[k:k+filter1.shape[0], i:i+filter1.shape[1],j]*filter1)
+                except IndexError:
+                    out[k,i]+=np.sum(img[k:k+filter1.shape[0], i:i+filter1.shape[1]]*filter1)
+        return out
+        
+a = CNN()
+im = plt.imread("plage.jpg")
+filterL=[np.array([[-1,0,-1],[0,0,0],[-1,0,-1]])]
+filterL.append(np.array([[-1,0,1],[-1,0,1],[-1,0,1]]))
+filterL.append(np.array([[0,1,0],[1,-1,1],[0,1,0]]))
+
+filterL.append(np.array([[-1,0,1],[-1,0,1],[-1,0,1]]))
+filterL.append(np.ones((3,3)))
+filterL.append(np.array([[-1,-1,-1],[0,0,0],[1,1,1]]))
+#filterL.append(np.array([[-1,0,0,0,-1],[0,1,1,1,0],[0,1,-1,1,0],[0,1,1,1,0],[-1,0,0,0,-1]]))
+
+L= a.convolution(im, np.transpose(np.array(filterL), axes=(1,2,0)))
+
+
+for k in range(L.shape[2]):
+    plt.figure()
+    plt.imshow(L[:,:,k])
+    
+
+'''
+def filtre(k):
+    if k ==0:
+        return a.convOfSingleFilter(im, filterL[k])
+    print("filtre ",k)
+    return a.convOfSingleFilter(filtre(k-1), filterL[k])
+
+
+plt.imshow(a.pooling("m", np.array(im) , 7)[:,:,1])
+plt.figure()
+plt.imshow(a.pooling("m", np.array(im) , 7)[:,:,0])
+plt.figure()
+plt.imshow(a.pooling("m", np.array(im) , 7)[:,:,2])
+#plt.imshow(a.convOfSingleFilter(im, filterL[6]))
+'''
